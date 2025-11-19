86eb9e3d2d5a70318d3f0cdf1376ab523a24fe8c
nitanshi12 <nitanshi.agarwal@netsmartz.net>
2025-11-19 18:35:49 +0530

    added the comp

--- ChaiAurCode/ReactRouter/src/components/Home/Home.jsx
+++ ChaiAurCode/ReactRouter/src/components/Home/Home.jsx
@@ -32,12 +32,16 @@ export default function Home() {
                 </div>
 
                 <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                    <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
+                    {/* Option 1: Remove the img tag entirely if you don't have an image */}
+                    {/* Option 2: Add a valid image source */}
+                    <img className="w-96" src="/path-to-your-image.jpg" alt="image1" />
                 </div>
             </aside>
 
             <div className="grid  place-items-center sm:mt-20">
-                <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
+                {/* Option 1: Remove the img tag entirely if you don't have an image */}
+                {/* Option 2: Add a valid image source */}
+                <img className="sm:w-96 w-48" src="/path-to-your-image2.jpg" alt="image2" />
             </div>
 
             <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
